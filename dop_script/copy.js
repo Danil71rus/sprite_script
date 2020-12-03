25d578c8cd67baaeb8fd56ebbdb04694d8fec513
Данил Степин <dstepin@itoolabs.com>
2020-12-03 22:34:28 +0300

    Доработка недочетов

--- dop_script/copy.js
+++ dop_script/copy.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-let spriteDir = "C:/Work/sprite_script/sprites/";
-let appDir = "C:/Work/sprite_script/result/"
+let spriteDir = "C:/Users/DonSt/Desktop/sprite_script/sprites/";
+let appDir = "C:/Work/ITLCS/itlcs/theme/";
 let dirList = fs.readdirSync(spriteDir);
 dirList.forEach(function (dir) {
     let dirPath = spriteDir + dir;
@@ -9,7 +9,7 @@ dirList.forEach(function (dir) {
     fileList.forEach(function (file) {
         let filePath = dirPath + "/" + file;
         console.log(`filePath: ${filePath}`)
-        let dstPath = appDir + file;
+        let dstPath = appDir + dir + "/" + file;
         console.log(`dstPath:  ${dstPath}`);
         fs.createReadStream(filePath).pipe(fs.createWriteStream(dstPath));
     });
--- dop_script/rename_dir.js
+++ dop_script/rename_dir.js
@@ -1,7 +1,8 @@
 //из такого названия папки "ito-avantelnsk-sprite_2020-12-03" в такое "avantelnsk"
+// var newName = dir.split("-")[1]; берет название между первым и вторым знаком "-"
 
 const fs = require('fs');
-let spriteDir = "C:/Work/sprite_script/sprites/";
+let spriteDir = "C:/Users/DonSt/Desktop/sprite_script/sprites/";
 let dirList = fs.readdirSync(spriteDir);
 dirList.forEach(function (dir) {
     let dirPath = spriteDir + dir;
